e675f716e4b4970fea2e27bef624b3505c5b2af4
f809814 <34770149+f809814@users.noreply.github.com>
2018-11-01 21:06:44 +0800

    第一次在github中修改

--- src/githubTest.java
+++ src/githubTest.java
@@ -7,5 +7,6 @@ public class githubTest {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
         System.out.println("第一次修改");
+        System.out.println("第一次在github中修改");
     }
 }
